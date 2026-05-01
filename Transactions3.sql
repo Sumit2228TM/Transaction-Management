b5864d8a7a938a0a174e6a9f38c57ce39baaa9b7
Sumit Gatade <sumitgatade05@gmail.com>
2026-05-01 12:31:24 +0530

    Add files via upload

---
+++ Transactions3.sql
@@ -0,0 +1,47 @@
+USE txn_lablast;
+
+-- Read Uncommitted 
+-- Session A uncommitted data Workbench B is CLI which will do dirty read
+SET SESSION TRANSACTION ISOLATION LEVEL READ UNCOMMITTED;
+SELECT * FROM bank_accounts;
+START TRANSACTION;
+UPDATE bank_accounts SET balance=balance-10000 WHERE account_id=1;	
+ROLLBACK;
+
+
+-- Read Committed 
+-- Session A uncommitted data Workbench B is CLI which will not read the updated values unless committed
+
+
+SET SESSION TRANSACTION ISOLATION LEVEL READ COMMITTED;
+SELECT * FROM bank_accounts;
+START TRANSACTION;
+UPDATE bank_accounts SET balance = balance + 10000 WHERE account_id=4;
+COMMIT;
+
+-- Repeatable Reads
+-- We get the same result, even if others commit changes i.e we same the same snapshot of data from the start of transaction & when u access it from outside you get new updated values
+
+SET SESSION TRANSACTION ISOLATION LEVEL REPEATABLE READ;
+SELECT * FROM bank_accounts;
+UPDATE bank_accounts SET balance= balance + 500 WHERE account_id =3;
+COMMIT;
+
+-- Serializable - Phantom Reads
+-- A new row can't appear in a repeated query inside a same transaction until one is reading it once it releases its locks the DML passes 
+
+SET SESSION TRANSACTION ISOLATION LEVEL SERIALIZABLE;
+
+START TRANSACTION;
+
+INSERT INTO bank_accounts (account_name, account_type, balance)
+VALUES ('Another User', 'savings', 2000.00);
+
+SELECT * FROM bank_accounts; -- No addition done of another user as CLI is reading it
+
+
+
+
+
+
+
